1e3b8b8ffb098a3e388d84a7eaf981e82dcfc46c
Destfolk <36056285+Destfolk@users.noreply.github.com>
2020-07-29 07:20:55 +0200

    Update SPI_Interface.vhd

--- SPI_Interface.vhd
+++ SPI_Interface.vhd
@@ -33,28 +33,44 @@ end SPI_Interface;
 
 architecture Behavioral of SPI_Interface is
     
-    signal Read_EN  : std_logic := '0';
-    signal Counter  : std_logic_vector (4 downto 0)  := "10111";
-    signal Data_Reg : std_logic_vector (23 downto 0) := (others => '0');
+    signal Read_EN   : std_logic := '0';
+    signal Counter_W : std_logic_vector (4 downto 0)  := "10111";
+    signal Counter_R : std_logic_vector (4 downto 0)  := "01111";
+    signal Data_Reg  : std_logic_vector (23 downto 0) := (others => '0');
      
     alias  WnR_bit  : std_logic is data_reg(23);
     
 begin
     ADDR          <= '0' & Data_Reg(22 downto 16);
     TEMP_DATA_OUT <= Data_Reg(15 downto  0);
-    SPI_OUT       <= TEMP_DATA_IN(index(Counter)) when Read_EN = '1' else '0';
+    SPI_OUT       <= TEMP_DATA_IN(index(Counter_R)) when Read_EN = '1' else '0';
         
-    Count : process(SPI_CLK)
+    Write_Counter : process(SPI_CLK)
+    begin
+        if rising_edge(SPI_CLK) then
+            if (SPI_EN = '1') then
+                if (Counter_W > "00000") then
+                    Counter_W <= Counter_W - 1;
+                else
+                    Counter_W <= "10111";
+                end if;
+            else
+                Counter_W <= "10111";
+            end if;                           
+        end if;   
+    end process;
+    
+    Read_Counter : process(SPI_CLK)
     begin
         if falling_edge(SPI_CLK) then
             if (SPI_EN = '1') then
-                if (Counter > "00000") then
-                    Counter <= Counter - 1;
+                if (Counter_W < "10000") then
+                    Counter_R <= Counter_R - 1;
                 else
-                    Counter <= "10111";
+                    Counter_R <= "10000";
                 end if;
             else
-                Counter <= "10111";
+                Counter_R <= "10000";
             end if;                           
         end if;   
     end process;
@@ -63,7 +79,7 @@ begin
     begin
         if rising_edge(SPI_CLK) then
             if (SPI_EN = '1') then
-                Data_Reg(index(Counter)) <= SPI_IN;
+                Data_Reg(index(Counter_W)) <= SPI_IN;
             else
                 Data_Reg <= (others => '0');
             end if;
@@ -73,7 +89,7 @@ begin
     Write_reg : process(SPI_CLK)
     begin
         if falling_edge(SPI_CLK) then
-            if (Counter = "00001" and WnR_bit = '1') then
+            if (Counter_W = "00001" and WnR_bit = '1') then
                 W_not_R <= '1';
             else
                 W_not_R <= '0';
@@ -84,7 +100,7 @@ begin
     Read_reg : process(SPI_CLK)
     begin
         if falling_edge(SPI_CLK) then
-            if (Counter < "10001" and Counter > "00000" and WnR_bit = '0') then
+            if (Counter_W < "10000" and Counter_W > "00000" and WnR_bit = '0') then
                 Read_EN <= '1';
             else
                 Read_EN <= '0';
