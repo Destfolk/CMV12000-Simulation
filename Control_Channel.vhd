e2f96ac645af969ebe2dfbdbc4722308c9f22e96
SeifEldeen Emad Abdalazeem <36056285+Destfolk@users.noreply.github.com>
2021-05-31 12:17:51 +0200

    Update Control_Channel.vhd

--- Control_Channel.vhd
+++ Control_Channel.vhd
@@ -4,54 +4,65 @@ use IEEE.NUMERIC_STD.all;
 use ieee.std_logic_unsigned.all;
 
 entity Control_Channel is
-    Port ( LVDS_CLK : in  std_logic;
-           IDLE     : in  std_logic;
-           DVAL     : in  std_logic;
-           LVAL     : in  std_logic;
-           FVAL     : in  std_logic;
-           FOT      : in  std_logic;
-           INTE1    : in  std_logic;
-           INTE2    : in  std_logic;
-           BIT_MODE : in  std_logic_vector(1 downto 0);
-           CTR_OUT  : out std_logic
+    Port ( LVDS_CLK       : in  std_logic;
+           IDLE           : in  std_logic;
+           OH             : in  std_logic; -- needs to lead by 0.5*T
+           Output_mode    : in  std_logic_vector(5  downto 0);
+           Channel_en     : in  std_logic_vector(2  downto 0);
+           Channel_en_bot : in  std_logic_vector(31 downto 0);
+           Channel_en_top : in  std_logic_vector(31 downto 0);
+           CC_out         : out std_logic_vector(11 downto 0)
           );
 end Control_Channel;
 
 architecture Behavioral of Control_Channel is
 
-    signal Ready   : std_logic := '0';
     signal Detect  : std_logic := '0';
-    signal CTR     : std_logic_vector(0 to 11);
-    signal Counter : std_logic_vector(3 downto 0);
+    signal CC      : std_logic_vector(2  downto 0);
+    signal Counter : std_logic_vector(5  downto 0) := (others => '0');
+    signal Enable  : std_logic_vector(31 downto 0);
     
 begin
-    
-    CD_Counter : process(LVDS_CLK)
-    begin  
+
+    Enable <= Channel_en_bot and Channel_en_bot;
+    CC_out(11 downto 3) <= "000010000";
+    CC_out(2  downto 0) <= CC;   
+     
+    process(LVDS_CLK)
+    begin
         if rising_edge(LVDS_CLK) then
-            if (IDLE = '1') then
-                Ready   <= '0';
-                Counter <= (others => '0');
-            else
-                Ready   <= '0';
-                Counter <= Counter + 1;
+            Detect <= OH;
             
-                case BIT_MODE is
-                    when "00" =>   
-                        if (Counter = "1011") then
-                            Counter <= (others => '0');
-                            Ready <= '1';
-                        end if; 
-                    when "01" =>
-                        if (Counter = "1001") then
-                            Counter <= (others => '0');
-                            Ready <= '1';
-                        end if; 
-                    when "10" =>
-                        if (Counter = "0111") then
-                            Counter <= (others => '0');
-                            Ready <= '1';
-                        end if; 
+            if (IDLE = '1') then
+                Counter <= (others => '0');
+            elsif (Detect = '1' and OH = '0') then
+                Counter <= Counter + 1;
+                
+                case Output_mode(4 downto 0) is
+                    when "00000" =>
+                        if (Counter = "000001") then
+                            Counter <= "000001";
+                        end if;
+                    when "00001" =>
+                        if (Counter = "000010") then
+                            Counter <= "000001";
+                        end if;
+                    when "00011" =>
+                        if (Counter = "000100") then
+                            Counter <= "000001";
+                        end if;
+                    when "00111" =>
+                        if (Counter = "001000") then
+                            Counter <= "000001";
+                        end if;
+                    when "01111" =>
+                        if (Counter = "010000") then
+                            Counter <= "000001";
+                        end if;
+                    when "11111" =>
+                        if (Counter = "100000") then
+                            Counter <= "000001";
+                        end if;
                     when others =>
                         null;
                 end case;
@@ -59,30 +70,69 @@ begin
         end if;
     end process;
     
-    Control_Data : process(LVDS_CLK)
+    process(LVDS_CLK)
     begin
         if rising_edge(LVDS_CLK) then
-            Detect <= IDLE;
+            if (Channel_en(1) = '0' or Enable = 0) then
+                CC <= "000";
+            elsif (IDLE = '1') then
+                CC <= "000";
+            elsif (OH = '1') then
             
-            if (Detect = '0' and IDLE = '1') then
-                CTR <= "000000010000";
-            elsif ((Detect = '1' and IDLE = '0') or ready = '1') then
-                CTR <= DVAL & LVAL & FVAL & FOT & INTE1 & INTE2 & "010000";
-            else     
-                case BIT_MODE is
-                        when "00" =>
-                                CTR <= CTR(1 to 11) & CTR(0);
-                        when "01" =>
-                                CTR(0 to 9) <= CTR(1 to 9) & CTR(0);
-                        when "10" =>
-                                CTR(0 to 7) <= CTR(1 to 7) & CTR(0);
-                        when others =>
-                            null;
+                case Output_mode(4 downto 0) is
+                    when "00000" =>
+                        if (Counter = "000000") then
+                            CC <= "000";
+                        else
+                            CC <= "100";
+                        end if;
+                    when "00001" =>
+                        if (Counter = "000000") then
+                            CC <= "000";
+                        elsif (Counter = "000010") then
+                            CC <= "100";
+                        else
+                            CC <= "110";
+                        end if;
+                    when "00011" =>
+                        if (Counter = "000000") then
+                            CC <= "000";
+                        elsif (Counter = "000100") then
+                            CC <= "100";
+                        else 
+                            CC <= "110";
+                        end if;
+                    when "00111" =>
+                        if (Counter = "000000") then
+                            CC <= "000";
+                        elsif (Counter = "001000") then
+                            CC <= "100";
+                        else 
+                            CC <= "110";
+                        end if;
+                    when "01111" =>
+                        if (Counter = "000000") then
+                            CC <= "000";
+                        elsif (Counter = "010000") then
+                            CC <= "100";
+                        else 
+                            CC <= "110";
+                        end if;
+                    when "11111" =>
+                        if (Counter = "000000") then
+                            CC <= "000";
+                        elsif (Counter = "100000") then
+                            CC <= "100";
+                        else 
+                            CC <= "110";
+                        end if;
+                    when others =>
+                        null;
                 end case;
+            else
+                CC <= "111";
             end if;
         end if;
     end process;
-    
-    CTR_OUT <= CTR(0);
     
 end Behavioral;
