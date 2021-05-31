cffee3e2a7cb3872e85440d8043b51a57e5ff82e
SeifEldeen Emad Abdalazeem <36056285+Destfolk@users.noreply.github.com>
2021-05-31 15:24:49 +0200

    Update Data_Training.vhd

--- Data_Training.vhd
+++ Data_Training.vhd
@@ -10,7 +10,7 @@ entity Data_Training is
            LVAL     : in  std_logic;
            fin_set  : in  std_logic;
            Bit_mode : in  std_logic_vector(1  downto 0);
-           TP1      : in  std_logic_vector(11 downto 0);
+           TP1_data : in  std_logic_vector(11 downto 0);
            TP_out   : out std_logic_vector(11 downto 0)
           );
 end Data_Training;
@@ -19,6 +19,7 @@ architecture Behavioral of Data_Training is
 
     signal TP1_EN     : std_logic;
     signal TP2_EN     : std_logic;
+    signal TP1        : std_logic_vector(11 downto 0);
     signal TP2        : std_logic_vector(11 downto 0);
     
 begin
@@ -28,9 +29,9 @@ begin
     
     process(LVDS_CLK)
     begin
-        
         if rising_edge(LVDS_CLK) then
             if (IDLE = '1' or TP1_EN = '1') then
+                TP1    <= TP1_data;
                 TP_out <= TP1;
             elsif (TP2_EN = '1') then
                 if (fin_set = '0') then
@@ -40,18 +41,20 @@ begin
                     
                     case Bit_mode is
                         when "00" =>
-                            TP2(11 downto 0) <= TP2(10 downto 0) & TP2(11);
+                            TP1(11 downto 0) <= TP1(10 downto 0) & TP1(11);
+                            TP2(11 downto 0) <= TP2(0) & TP2(11 downto 1);
                         when "01" =>
-                            TP2(9  downto 0) <= TP2(8  downto 0) & TP2(9);
+                            TP1(9  downto 0) <= TP1(8  downto 0) & TP1(9);
+                            TP2(9  downto 0) <= TP2(0) & TP2(9 downto 1);
                         when "10" =>
-                            TP2(7  downto 0) <= TP2(6  downto 0) & TP2(7);
+                            TP1(7  downto 0) <= TP1(6  downto 0) & TP1(7);
+                            TP2(7  downto 0) <= TP2(0) & TP2(7 downto 1);
                         when others =>
                             null;
                     end case;
                 end if;
             end if;
         end if;
-        
     end process;
     
 end Behavioral;
